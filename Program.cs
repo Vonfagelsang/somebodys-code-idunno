435128d41f086303006ec53691c8a9156f83f7dc
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2021-10-19 15:43:31 +0200

    idunno somebodies geussing program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace gissa_spel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump=new Random();
+            int rand = slump.Next(100);
+            int tries = 0;
+            Console.WriteLine("Gissa ett tal:");
+            int num=int.Parse(Console.ReadLine());
+            while (num != rand)
+            {
+               if (num < rand)
+               {
+                   Console.WriteLine("Gissningen är för låg");
+               } 
+               if (num > rand)
+               {
+                   Console.WriteLine("Gissningen är för hög");
+               }
+               Console.WriteLine("Gissa ett tal:");
+               num=int.Parse(Console.ReadLine());
+               tries += 1;
+            }
+            Console.WriteLine("Det är rätt tal");
+            Console.WriteLine("Antal försök är " + tries);
+
+        }
+    }
+}
